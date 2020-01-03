c9c3da0868dd024368a4ee945b62a6bf61882726
cocoshen89 <57342384+cocoshen89@users.noreply.github.com>
2020-01-04 00:05:51 +0800

    Update 5-Problem.cpp

--- 5-Problem.cpp
+++ 5-Problem.cpp
@@ -1,1 +1,20 @@
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30754
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int month;
+    while(cin >> month)
+    {
+        if(3 <= month && month <= 5)
+            cout << "Spring\n";
+        else  if(6 <= month && month <= 8)
+            cout << "Summer\n";
+        else  if(9 <= month && month <= 11)
+            cout << "Autumn\n";
+        else
+            cout << "Winter\n";
+    }
+    return 0;
+}
