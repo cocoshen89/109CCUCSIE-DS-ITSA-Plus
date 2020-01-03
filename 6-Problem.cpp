a8061ecdba00b21306de5d9ab896d170fd8da8f6
cocoshen89 <57342384+cocoshen89@users.noreply.github.com>
2020-01-04 00:06:37 +0800

    Update 6-Problem.cpp

--- 6-Problem.cpp
+++ 6-Problem.cpp
@@ -1,1 +1,40 @@
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30755
+#include<iostream>
+#include<utility>
+using namespace std;
+
+int main()
+{
+    int nu;
+    cin >> nu;
+    for(int i=0;i<nu;i++)
+    {
+        char use;
+        cin >> use;
+        pair<int,int> z1,z2,ans;
+        int a,b;
+        cin >> a >> b;
+        z1=make_pair(a,b);
+        cin >> a >> b;
+        z2=make_pair(a,b);
+        if(use == '+')
+        {
+            ans.first=z1.first+z2.first;
+            ans.second=z1.second+z2.second;
+        }
+        else if(use == '-')
+        {
+            ans.first=z1.first-z2.first;
+            ans.second=z1.second-z2.second;
+        }
+        else
+        {
+            ans.first=z1.first*z2.first-z1.second*z2.second;
+            ans.second=z1.first*z2.second+z2.first*z1.second;
+        }
+        cout << ans.first << " " << ans.second << "\n";
+
+    }
+    return 0;
+}
 
