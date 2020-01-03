df37a04a301ae220ee3c3d2aa18a1dce987aea49
cocoshen89 <57342384+cocoshen89@users.noreply.github.com>
2020-01-04 00:05:08 +0800

    Update 4-Problem.cpp

--- 4-Problem.cpp
+++ 4-Problem.cpp
@@ -1,1 +1,46 @@
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30750
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int nu,mark;
+    int bin_nu[8];
+    int index;
+    while(cin >> nu)
+    {
+        for(int i=0;i<8;i++)
+        {
+            bin_nu[i]=0;
+        }
+        index=0;
+        mark=0;
+        if(nu < 0)
+        {
+            mark=1;
+            nu=-(nu+1);
+        }
+        while(nu>0)
+        {
+            bin_nu[index]=(nu%2);
+            nu/=2;
+            index++;
+        }
+        for(int i=7;i>=0;i--)
+        {
+            if(mark == 1)
+            {
+                if(bin_nu[i] == 0)
+                    cout << "1";
+                else
+                    cout << "0";
+            }
+            else
+                cout << bin_nu[i];
+        }
+        cout << "\n";
+    }
+    return 0;
+}
 
