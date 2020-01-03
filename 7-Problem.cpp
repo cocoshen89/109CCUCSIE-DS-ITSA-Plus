c182422820ff762caf787f9464447442d1258659
cocoshen89 <57342384+cocoshen89@users.noreply.github.com>
2020-01-04 00:07:21 +0800

    Update 7-Problem.cpp

--- 7-Problem.cpp
+++ 7-Problem.cpp
@@ -1,1 +1,26 @@
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30752
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int nu,mark;
+    while(cin >> nu)
+    {
+        mark=0;
+        for(int i=2;i*i<=nu;i++)
+        {
+            if(nu%i == 0)
+            {
+                mark=1;
+                break;
+            }
+        }
+        if(mark == 0)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
 
