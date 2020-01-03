11771df1a80cf3fc69cab550d1e6476e92291b04
cocoshen89 <57342384+cocoshen89@users.noreply.github.com>
2020-01-04 00:08:04 +0800

    Update 8-Problem.cpp

--- 8-Problem.cpp
+++ 8-Problem.cpp
@@ -1,1 +1,16 @@
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=30751
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int nu;
+    while(cin >> nu)
+    {
+        int longen=nu/3;
+        int ans=(3+longen*3)*longen/2;
+        cout << ans << "\n";
+    }
+    return 0;
+}
 
